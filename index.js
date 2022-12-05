2e3cb29691536ad640a23637ca1c493697a805f2
Robert Allende <allende.rob@gmail.com>
2022-12-04 23:10:57 -0600

    commit tree bb7235b0f53977c3a377c09b18434e3bbf971c1b

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your code here
+
+
+
+
+function submitData (userName, userEmail) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": 'application/json',
+            "Accept": 'application/json',
+        },
+         body: JSON.stringify({
+            name: `${userName}`,
+            email: `${userEmail}`,
+        }),
+    })
+
+    // .then(resp => resp.json())
+    // .then(data => {
+    //     let line = document.createElement('li')
+    //     line.textContent = data.id
+    //     document.body.append(line)
+    // })
+
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        let line = document.createElement('li')
+        line.textContent = data.id
+        document.body.append(line)
+    })
+    .catch(error => {
+        let mistake = document.createElement('li')
+        mistake.textContent = error.message
+        document.body.append(mistake)
+    })
+}
